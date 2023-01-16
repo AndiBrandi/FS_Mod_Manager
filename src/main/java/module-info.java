ee198ce84fafeac7a0490905794e2c7e468b2aca
AndiBrandi <andi.brandi@outlook.com>
2023-01-16 17:29:01 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.modmanager.fsmodmanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.modmanager.fsmodmanager to javafx.fxml;
+    exports com.modmanager.fsmodmanager;
+}
