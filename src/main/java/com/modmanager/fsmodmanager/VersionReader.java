c23d09ca480089138c3fcaee4c2680b5c1e68c67
Andreas <andreasschicho@hotmail.com>
2023-01-23 14:19:19 +0100

    Performance boost with the versions

--- src/main/java/com/modmanager/fsmodmanager/VersionReader.java
+++ src/main/java/com/modmanager/fsmodmanager/VersionReader.java
@@ -1,29 +1,14 @@
 package com.modmanager.fsmodmanager;
 
 import java.io.*;
-import java.util.List;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.xpath.*;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-import static java.lang.System.in;
 
 
 public class VersionReader {
@@ -31,8 +16,9 @@ public class VersionReader {
 
         //System.out.println(getVersion("C:/Users/andre/Documents/My Games/FarmingSimulator2022/mods/FS22_WestBridgeHills22.zip"));                                                             //Just for Testing
     }
-    public static String getVersion(String pathname){
-            File f = new File(pathname);
+
+    public static String getVersion(String pathname) {
+        File f = new File(pathname);
         FileInputStream fis = null;
         try {
             fis = new FileInputStream(f);
@@ -40,43 +26,41 @@ public class VersionReader {
             throw new RuntimeException(e);
         }
         BufferedInputStream bfis = new BufferedInputStream(fis);
-            ZipInputStream zis = new ZipInputStream(bfis);
+        ZipInputStream zis = new ZipInputStream(bfis);
 
-            ZipEntry zipEntry;
-            //int numEntry = 0;                                                                                                                                                                 //Needed for Stats
-            while (true) {
-                try {
-                    if (!((zipEntry = zis.getNextEntry()) != null)) break;
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                //numEntry++;                                                                                                                                                                   //Needed for Stats
-                if(zipEntry.getName().equals("modDesc.xml")) {
-                    //System.out.format("Entry #%d: path=%s, size=%d, compressed size=%d \n", numEntry, zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize());                  //Stats
-                    byte[] buffer = new byte[2048];
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    int len;
-                    // read bytes of file
-                    while (true) {
-                        try {
-                            if (!((len = zis.read(buffer)) > 0)) break;
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        //System.out.println("file: " + zipEntry.getName() + " : " + len + "  bytes read");
-                        bos.write(buffer, 0, len);
-                    }
-                    // convert bytes to string
-                    byte[] zipFileBytes = bos.toByteArray();
-                    String fileContent = new String(zipFileBytes);
-                    String teil1 = fileContent.substring(fileContent.indexOf("<version>"), fileContent.indexOf("</version>"));
-                    teil1 = teil1.substring(teil1.indexOf(">")+1);
-
-                    return teil1;
-                }
+        ZipEntry zipEntry;
+        //int numEntry = 0;                                                                                                                                                                 //Needed for Stats
+        //Inspect every file in the zip
+        while (true) {
+            try {
+                if (!((zipEntry = zis.getNextEntry()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-            // remember to close the zip input stream!
+            //numEntry++;                                                                                                                                                                   //Needed for Stats
+            if (zipEntry.getName().equals("modDesc.xml")) {
+                //System.out.format("Entry #%d: path=%s, size=%d, compressed size=%d \n", numEntry, zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize());                  //Stats
+                byte[] buffer = new byte[2048];
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int len;
+
+                // read bytes of file
+                while (true) {
+                    try {
+                        if (!((len = zis.read(buffer)) > 0)) break;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    bos.write(buffer, 0, len);
+                }
+                // convert bytes to string
+                byte[] zipFileBytes = bos.toByteArray();
+                String fileContent = new String(zipFileBytes);
+                String teil1 = fileContent.substring(fileContent.indexOf("<version>"), fileContent.indexOf("</version>"));
+                teil1 = teil1.substring(teil1.indexOf(">") + 1);
+                return teil1;
+            }
+        }
         try {
             zis.close();
         } catch (IOException e) {
